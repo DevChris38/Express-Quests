66603d26f6642a2b34395531f71244752add6bae
Christopher Arthaud <christopher.arthaud@gmail.com>
2023-11-26 22:23:41 +0100

    challenge succeded

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -1,47 +1,68 @@
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    color: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    color: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
+const movies = [];
+
+const database = require("../../database");
 
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const movie = movies.find((movie) => movie.id === id);
+  database
+    .query("select * from movies where id = ?", [id])
+    .then(([movies]) => {
+      if (movies.find((movie) => (movie.id = id))) {
+        res.json(movies.find((movie) => (movie.id = id)));
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send("Not Found");
+    });
+};
 
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const getUsersById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users.find((user) => (user.id = id))) {
+        res.json(users.find((user) => (user.id = id)));
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send("Not Found");
+    });
 };
 
 module.exports = {
   getMovies,
   getMovieById,
+  getUsers,
+  getUsersById,
 };
